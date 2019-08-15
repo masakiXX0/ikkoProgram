f45029308c966a05093c7b17af09b50dc97e3e92
masaki <muguet.cake@gmail.com>
2019-08-15 10:22:33 +0900

    カスタムキュークラスの基本メソッド群を追加

--- customQueue.py
+++ customQueue.py
@@ -2,3 +2,23 @@ class CustomQueue:
     def __init__(self, maxsize):
         self.queue = [] #FIFO
         self.maxsize = maxsize
+
+    # エンキュー
+    def enqueue(self, item):
+        if self.full():
+            self.dequeue()        
+        self.queue.append(item)
+
+    # デキュー
+    def dequeue(self):
+        return self.queue.pop(0)
+
+    # キューがいっぱい
+    def full(self):
+        if len(self.queue) == self.maxsize:
+            return True
+        return False
+    
+    # キュー内の文字を連結
+    def join_queue_string(self):
+        return "".join(self.queue)
